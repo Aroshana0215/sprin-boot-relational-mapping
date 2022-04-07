bd29b19640ecdafcdef99dc0b223082553ef7f71
aroshana <rasitharoshana0215@gmail.com>
2022-04-07 13:14:58 +0530

    First commit

---
+++ src/main/java/com/example/relationalMappingMtM/RelationalMappingMtMApplication.java
@@ -0,0 +1,13 @@
+package com.example.relationalMappingMtM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationalMappingMtMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelationalMappingMtMApplication.class, args);
+	}
+
+}
